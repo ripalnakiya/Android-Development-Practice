f3abe124cf4b72c8accd52196dc36a103d2e949f
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2023-10-30 19:03:35 +0530

    Changes in Notification

--- 22_Notification/app/src/main/java/com/ripalnakiya/a22notification/MainActivity.java
+++ 22_Notification/app/src/main/java/com/ripalnakiya/a22notification/MainActivity.java
@@ -1,60 +1,74 @@
 package com.ripalnakiya.a22notification;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    NotificationManager notificationManager;
+    String CHANNEL_ID = "myChannel";
+    int NOTIFICATION_ID = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
 
+        // Initializing Notification Manager
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // Creating Notification Channel
+        createNotificationChannel();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Creating and Displaying Notification
                 makeNotification();
             }
         });
     }
 
-//    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void makeNotification() {
-        String CHANNEL_ID = "myChannel";
-        int NOTIFICATION_ID = 11;
+    public void createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence name = "My Channel";
+            String description = "My Channel Description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = null;
 
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+            channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void makeNotification() {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.home)
                 .setContentTitle("New Notification")
                 .setContentText("Message from Home")
+                .setContentIntent(pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true);
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            CharSequence name = "My Channel";
-            String description = "Channel for my app notifications";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = null;
-            channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(channel);
-        }
+        Notification notification = builder.build();
 
-        // Get the NotificationManager and display the notification
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(NOTIFICATION_ID, notification.build());
-
+        // Display the notification
+        notificationManager.notify(NOTIFICATION_ID, notification);
     }
+
 }
