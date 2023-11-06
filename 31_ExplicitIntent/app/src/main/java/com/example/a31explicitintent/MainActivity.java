6666b51ff51f0d10a09f0ce09871efc2f549c816
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2023-11-06 16:35:34 +0530

    Opening Another App Activity using Package Name

---
+++ 31_ExplicitIntent/app/src/main/java/com/example/a31explicitintent/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.a31explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonYoutube = findViewById(R.id.buttonYoutube);
+
+        buttonYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.youtube.com/watch?v=2hIY1xuImuQ"));
+                intent.setPackage("com.google.android.youtube");
+                startActivity(intent);
+            }
+        });
+    }
+}
