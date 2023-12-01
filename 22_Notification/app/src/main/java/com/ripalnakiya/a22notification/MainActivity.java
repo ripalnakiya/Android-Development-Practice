6ae2b389d60592be13bce348b47c8d98857a81df
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2023-12-01 11:07:07 +0530

    Updated 22 Notification

--- 22_Notification/app/src/main/java/com/ripalnakiya/a22notification/MainActivity.java
+++ 22_Notification/app/src/main/java/com/ripalnakiya/a22notification/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,61 +15,85 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     NotificationManager notificationManager;
-    String CHANNEL_ID = "myChannel";
-    int NOTIFICATION_ID = 1;
+    String CHANNEL_ID = "101";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
 
-        // Initializing Notification Manager
+        Button mainButton = findViewById(R.id.mainButton);
+        Button secondButton = findViewById(R.id.secondButton);
+
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        // Creating Notification Channel
+
         createNotificationChannel();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Creating and Displaying Notification
-                makeNotification();
+                Notification notification = makeMainActivityNotification();
+                displayNotification(notification);
+            }
+        });
+
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification notification = makeSecondActivtyNotification();
+                displayNotification(notification);
             }
         });
     }
 
     public void createNotificationChannel() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            CharSequence name = "My Channel";
-            String description = "My Channel Description";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = null;
+            NotificationChannel notificationChannel = new NotificationChannel( CHANNEL_ID, "NEWS", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Get latest news");
+            notificationChannel.setShowBadge(true);
 
-            channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-
-            notificationManager.createNotificationChannel(channel);
+            notificationManager.createNotificationChannel(notificationChannel);
         }
     }
 
-    public void makeNotification() {
+    public Notification makeMainActivityNotification() {
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.home)
-                .setContentTitle("New Notification")
-                .setContentText("Message from Home")
+                .setContentTitle("Main Activity")
+                .setContentText("This notification will redirect you to Main Activity")
                 .setContentIntent(pendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true);
 
-        Notification notification = builder.build();
+        return builder.build();
+    }
 
-        // Display the notification
-        notificationManager.notify(NOTIFICATION_ID, notification);
+    public Notification makeSecondActivtyNotification() {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        // Without Backstack
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        // With Backstack
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+        taskStackBuilder.addNextIntentWithParentStack(intent);
+        PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(1, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.home)
+                .setContentTitle("Second Activity")
+                .setContentText("This notification will redirect you to Second Activity")
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        return builder.build();
+    }
+
+    public void displayNotification(Notification notification) {
+        notificationManager.notify(12, notification);
     }
 
 }
